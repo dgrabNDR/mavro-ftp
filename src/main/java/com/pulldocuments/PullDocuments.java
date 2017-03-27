50183b27c585274efa5f699a4991589edde3345c
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-03-27 12:00:19 -0400

    Add files via upload

--- src/main/java/com/pulldocuments/PullDocuments.java
+++ src/main/java/com/pulldocuments/PullDocuments.java
@@ -45,52 +45,49 @@ public class PullDocuments extends HttpServlet{
 		parameters.putAll(CredentialManager.getLogin());
 		this.params = parameters;
 		System.out.println("parsing request body parameters...");	
-		/*
-		// login to salesforce and pull attachment
-		sc = new SalesforceConnector(params.get("Username"),params.get("Password"),params.get("environment"));
-		ArrayList<SObject> attachments = new ArrayList<SObject>();		
-		try {	
-			System.out.println("logging into salesforce...");
-			sc.login();
-			attachments = query(params.get("attId"));
-			System.out.println("queried "+attachments.size()+" attachments");	
-		} catch (ConnectionException e1) {
-			e1.printStackTrace();
-		}
 		
-		ArrayList<SObject> encryptedSObjs = new ArrayList<SObject>();
-		ArrayList<File> encryptedFiles = new ArrayList<File>();
-		
-		// encrypt file
-		System.out.println("encrypting attachment...");
-		for(SObject so : attachments){			
-			//EncryptFile.writeToFile((String)so.getField("Name"), (String)so.getField("Body"));
-			try {
-				EncryptFile ef = new EncryptFile();
-				File suchEncrypt = ef.encrypt(base64ToByte((String)so.getField("Body")),(String)so.getField("Name"));							
-				encryptedSObjs.add(fileToSObj((String)so.getField("ParentId"),(String)so.getField("Name")+".pgp",suchEncrypt));
-				encryptedFiles.add(suchEncrypt);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}	
-		}
-		
-		// add attachment to report in salesforce
-		System.out.println("adding salesforce attachment...");
-		try {
-			sc.create(encryptedSObjs);
-		} catch (ConnectionException e) {
-			e.printStackTrace();
-		}
-	*/
 		// upload files to ida ftp		
-		UploadFile uf = new UploadFile();
-		uf.start(params, null);
+		SFTPConnector connector = new SFTPConnector();
+		connector.start(params);
 		try{
-			uf.upload();
-		} finally {
-			System.out.println("did it work? "+uf.res == null);
-		}
+			connector.connect();
+			// do the things
+			
+			/*
+			// login to salesforce and pull attachment
+			sc = new SalesforceConnector(params.get("Username"),params.get("Password"),params.get("environment"));
+			ArrayList<SObject> attachments = new ArrayList<SObject>();		
+			try {	
+				System.out.println("logging into salesforce...");
+				sc.login();
+				attachments = query(params.get("attId"));
+				System.out.println("queried "+attachments.size()+" attachments");	
+			} catch (ConnectionException e1) {
+				e1.printStackTrace();
+			}
+			
+			// create new SF attachment objects
+			ArrayList<SObject> lstSObj = new ArrayList<SObject>();
+			for(SObject so : attachments){			
+				try {						
+					lstSObj.add(fileToSObj((String)so.getField("ParentId"),(String)so.getField("Name")+".pgp",attachments));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}	
+			}
+			
+			// add attachment to report in salesforce
+			System.out.println("adding salesforce attachment...");
+			try {
+				sc.create(lstSObj);
+			} catch (ConnectionException e) {
+				e.printStackTrace();
+			}
+			*/
+			
+			connector.disconnect();
+		} finally {}
+		
 	}
 	
 	private SObject fileToSObj(String pId, String fileName, File theFile){
@@ -106,16 +103,6 @@ public class PullDocuments extends HttpServlet{
 		
 		sObj.setField("Body", body);
 		return sObj;
-	}
-	
-	private File newFile(String fileName, byte[] body) throws FileNotFoundException, IOException{
-		File theFile = new File(fileName);
-		if(theFile.createNewFile()) {
-        	FileOutputStream fos = new FileOutputStream(theFile);
-        	fos.write(body);
-        	fos.close();
-        }
-		return theFile;
 	}
 	
 	private String getBody(HttpServletRequest req) throws IOException{
---
+++ src/main/java/com/pulldocuments/SFTPConnector.java
@@ -0,0 +1,153 @@
+package main.java.com.pulldocuments;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Authenticator;
+import java.net.InetAddress;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import main.java.com.salesforce.SalesforceConnector;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.SftpException;
+import com.sforce.soap.partner.sobject.SObject;
+import com.sforce.ws.ConnectionException;
+
+public class SFTPConnector {
+	public Map<String,String> params = new HashMap<String,String>();
+	public Boolean res;
+	public ChannelSftp sftpChannel;
+	public Session session;
+
+	
+	public void start(Map<String,String> p){
+		System.out.println("starting uploader...");
+		params = p;
+	}
+	
+	public void connect(){
+		
+		try{
+			URL connection = new URL("http://checkip.amazonaws.com/");
+			URLConnection con = connection.openConnection();
+			String str = null;
+			BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			str = reader.readLine();
+			System.out.println("ip before fixie: "+str);
+			
+			// sets static ip addresses 
+			System.out.println("Normal IP Address => "+InetAddress.getLocalHost().getHostAddress());
+			System.out.println("setting up fixie...");
+			URL fixie = new URL("http://"+System.getenv("FIXIE_SOCKS_HOST"));
+			String fixieInfo = fixie.getUserInfo();
+			System.out.println("fixieURL ==> "+fixieInfo);
+			String[] fixieUserInfo = fixieInfo.split(":");
+			String fixieUser = fixieUserInfo[0];
+			String fixiePassword = fixieUserInfo[1];
+			System.out.println("fixie u: "+fixieUser+" ,p: "+fixiePassword);
+			System.setProperty("socksProxyHost", fixie.getHost());
+			Authenticator.setDefault(new ProxyAuthenticator(fixieUser, fixiePassword));
+
+			connection = new URL("http://checkip.amazonaws.com/");
+			con = connection.openConnection();
+			str = null;
+			reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			str = reader.readLine();
+			System.out.println("ip after fixie: "+str);
+			
+			JSch jsch = new JSch();
+	        session = null;
+	        try {
+	            session = jsch.getSession(params.get("ftpuser"), params.get("ftphost"), 22);
+	            Properties prop = new Properties();
+	            prop.setProperty("StrictHostKeyChecking", "no");
+	            session.setConfig(prop);
+	            session.setPassword(params.get("ftppass"));	            
+	            System.out.println("user: "+params.get("ftpuser")+" / pass: "+params.get("ftppass")+" / host: "+params.get("ftphost"));
+	            System.out.println("connecting to session...");
+	            session.connect();
+	            Channel channel = session.openChannel("sftp");
+	            System.out.println("connecting to channel...");
+	            channel.connect();
+	            sftpChannel = (ChannelSftp) channel;
+	            System.out.println("connected!  pwd: "+sftpChannel.pwd());
+	            //return sftpChannel;
+	        } catch (JSchException e) {
+	            e.printStackTrace();  
+	        }
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public void disconnect(){
+		sftpChannel.exit();
+        session.disconnect();
+	}
+	
+	private static void showServerReply(FTPClient ftpClient) {
+        String[] replies = ftpClient.getReplyStrings();
+        if (replies != null && replies.length > 0) {
+            for (String aReply : replies) {
+                System.out.println("SERVER: " + aReply);
+            }
+        }
+    }
+
+	private class ProxyAuthenticator extends Authenticator {
+	  private final PasswordAuthentication passwordAuthentication;
+	  private ProxyAuthenticator(String user, String password) {
+	    passwordAuthentication = new PasswordAuthentication(user, password.toCharArray());
+	  }
+
+	  @Override
+	  protected PasswordAuthentication getPasswordAuthentication() {
+	    return passwordAuthentication;
+	  }
+	}
+	
+	public byte[] readFile(File file) throws IOException{
+		InputStream is = new FileInputStream(file);
+		long length = file.length();
+		byte[] bytes = new byte[(int)length];
+		int offset = 0;
+        int numRead = 0;
+        while (offset < bytes.length && (numRead=is.read(bytes, offset, bytes.length-offset)) >= 0) {
+            offset += numRead;
+        }
+        return bytes;
+	}
+	
+	public byte[] base64ToByte(String data) throws Exception {
+		return Base64.decodeBase64(data.getBytes());
+	}
+}
