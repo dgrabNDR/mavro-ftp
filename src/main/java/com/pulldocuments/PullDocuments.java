222347d6cce9e25502edca30cc46c817c18dca74
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-03-28 15:10:07 -0400

    push

--- src/main/java/com/pulldocuments/PullDocuments.java
+++ src/main/java/com/pulldocuments/PullDocuments.java
@@ -14,6 +14,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
