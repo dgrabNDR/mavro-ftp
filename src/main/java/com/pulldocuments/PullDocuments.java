e9f67ac8c87c1dc841fedc814d24bf405f465997
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-04-05 15:10:28 -0400

    push

--- src/main/java/com/pulldocuments/PullDocuments.java
+++ src/main/java/com/pulldocuments/PullDocuments.java
@@ -125,23 +125,22 @@ public class PullDocuments extends HttpServlet{
 					}
 				}
 				// move day folder to MavroArchive	
-				connector.sftpChannel.cd("/E:/Opex/Mavro/"+dayFolder.getFilename());
+				connector.sftpChannel.cd("/E:/Opex/Mavro/");
 				Vector<ChannelSftp.LsEntry> lstFolder = connector.sftpChannel.ls("*");
-				System.out.println("folder contents: "+lstFolder.size());
+				System.out.println("/E:/Opex/Mavro/ folder contents: "+lstFolder.size());
 				for(ChannelSftp.LsEntry fld : lstFolder){
 					System.out.println(fld.getFilename());
-				}
-				if(lstFolder.size() == 0){
-					System.out.println("dayFolder is empty.  deleting...");
-					connector.sftpChannel.cd("/E:/Opex/Mavro/");
-					SftpATTRS attrs = null;
-					try {
-					    attrs = connector.sftpChannel.stat("/E:/Opex/Mavro/"+dayFolder.getFilename());
-					} catch (Exception e) {
-					    System.out.println("/E:/Opex/Mavro/"+dayFolder.getFilename()+" not found");
-					}
-					if(attrs != null) {
-						connector.sftpChannel.rm("/E:/Opex/Mavro/"+dayFolder.getFilename());
+					if(fld.getFilename() == dayFolder.getFilename()){
+						System.out.println("dayFolder is empty.  deleting...");
+						SftpATTRS attrs = null;
+						try {
+						    attrs = connector.sftpChannel.stat("/E:/Opex/Mavro/"+dayFolder.getFilename());
+						} catch (Exception e) {
+						    System.out.println("/E:/Opex/Mavro/"+dayFolder.getFilename()+" not found");
+						}
+						if(attrs != null) {
+							connector.sftpChannel.rm("/E:/Opex/Mavro/"+dayFolder.getFilename());
+						}
 					}
 				}
 			}
